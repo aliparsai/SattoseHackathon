a0011080bde89d70cbdaec1ca3199ce9b8f2a5c5
Ali Parsai <ali.parsai@email.com>
2015-07-07 15:07:21 +0200

    "triggercommit-1b888ee2985a42d78e407dba52a6719f"

--- fetch-bib.sh
+++ fetch-bib.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 
 curl -o tmp-reference.bib http://dblp.uni-trier.de/pers/tb2/d/Demeyer:Serge.bib
-echo -e "---\n---\nReferences\n=============\n\n" > reference.bib
+echo "---\n---\nReferences\n=============\n\n" > reference.bib
 cat tmp-reference.bib >> reference.bib
 rm tmp-reference.bib
