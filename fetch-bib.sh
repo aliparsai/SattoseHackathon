7adf08828fcc34d2942ca52abdd77aec5031f124
Ali Parsai <ali.parsai@email.com>
2015-07-07 15:01:56 +0200

    fixed front matter addition

--- fetch-bib.sh
+++ fetch-bib.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-curl -o reference.bib http://dblp.uni-trier.de/pers/tb2/d/Demeyer:Serge.bib
+curl -o tmp-reference.bib http://dblp.uni-trier.de/pers/tb2/d/Demeyer:Serge.bib
+echo -e "---\n---\nReferences\n=============\n\n" > reference.bib
+cat tmp-reference.bib >> reference.bib
+rm tmp-reference.bib
