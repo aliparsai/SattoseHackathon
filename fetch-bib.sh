eb6a4f4068bf27ed231843de807bc98b7576ade8
Ali Parsai <ali.parsai@email.com>
2015-07-07 15:27:19 +0200

    "triggercommit-012fb70adaa3409cb9a65caeb4bbd389"

--- fetch-bib.sh
+++ fetch-bib.sh
@@ -3,4 +3,5 @@
 curl -o tmp-reference.bib http://www.parsai.net/files/research/Mutation%20Testing%20as%20a%20Safety%20Net%20for%20Test%20Code%20Refactoring.bib
 echo "---\n---\nReferences\n=============\n\n" > reference.bib
 cat tmp-reference.bib >> reference.bib
+sed -i '/^%/d' reference.bib
 rm tmp-reference.bib
