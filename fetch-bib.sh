9249f9a41f99fd9ab4fc1fbcbf09cb32f7ad0109
Ali Parsai <ali.parsai@email.com>
2015-07-07 15:11:40 +0200

    changed author

--- fetch-bib.sh
+++ fetch-bib.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-curl -o tmp-reference.bib http://dblp.uni-trier.de/pers/tb2/d/Demeyer:Serge.bib
+curl -o tmp-reference.bib http://dblp.uni-trier.de/pers/tb2/p/Parsai:Ali.bib
 echo "---\n---\nReferences\n=============\n\n" > reference.bib
 cat tmp-reference.bib >> reference.bib
 rm tmp-reference.bib
